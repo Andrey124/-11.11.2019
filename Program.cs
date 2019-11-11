32d8d9ae46b732dde842d99d0721bd5394f2499c
Andrey124 <56398208+Andrey124@users.noreply.github.com>
2019-11-11 09:54:09 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите длину окружности");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Площадь круга,ограниченного этой окружности равна : {0}", (d * d) / (4 * Math.PI));
+            Console.ReadKey();  
+        }
+    }
+}
